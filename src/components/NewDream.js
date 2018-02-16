7c2d6c5111543e8bfd91cbc33557873da8d5c214
Alex Cassell <alex@simulism.net>
2018-02-15 22:48:03 -0500

    Still working in the interface.. started adding harded code post tags

--- src/components/NewDream.js
+++ src/components/NewDream.js
@@ -28,7 +28,26 @@ class NewDream extends Component {
                 </label>
             </form>
             <div className="TagsWrapper">
-                <button className="Tags" title="Word">Home</button>
+                <div className="Subtitle">Actual Location</div>
+                <div className="TagsWrappers__Inner">
+                    <button className="Tags" title="Home">Home</button><button className="Tags" title="Partner's">Partner's</button>
+                    <button className="Tags" title="Friends's">Friends's</button><button className="Tags" title="Parents's">Parents's</button>
+                    <button className="Tags" title="Hotel">Hotel</button><button className="Tags" title="Hospital">Hospital</button>
+                    <button className="Tags" title="School">School</button><button className="Tags" title="Other">Other</button>
+                </div>
+                <div className="Subtitle">Dream Location</div>
+                <div className="TagsWrappers__Inner">
+                    <button className="Tags" title="Home">Home</button><button className="Tags" title="Partner's">Partner's</button>
+                    <button className="Tags" title="Friends's">Friends's</button><button className="Tags" title="Parents's">Parents's</button>
+                    <button className="Tags" title="Hotel">Hotel</button><button className="Tags" title="Hospital">Hospital</button>
+                    <button className="Tags" title="School">School</button><button className="Tags" title="Work">Work</button>
+                    <button className="Tags" title="Recurring">Recurring</button><button className="Tags" title="Garage">Garage</button>
+                    <button className="Tags" title="Street">Street</button><button className="Tags" title="Bridge">Bridge</button>
+                    <button className="Tags" title="Car">Car</button><button className="Tags" title="Bus">Bus</button>
+                    <button className="Tags" title="Plane">Plane</button><button className="Tags" title="Forest">Forest</button>
+                    <button className="Tags" title="Planet">Planet</button><button className="Tags" title="Moon">Moon</button>
+                    <button className="Tags" title="Space">Space</button><button className="Tags" title="Other">Other</button>
+                </div>
             </div>
             <input type="submit" value="Submit" />
         </div>
